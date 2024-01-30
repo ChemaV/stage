7afb8f213c72755122e1bf9a5280ca1d237d68fb
ChemaV <quemavic@gmail.com>
2024-01-30 14:23:00 +0100

    Push index

--- main.js
+++ main.js
@@ -44,23 +44,5 @@ loader.load( 'fonts/helvetiker_regular.typeface.json', function ( font ) {
 
 
 
-
-export function Model(props) {
-  const { nodes, materials } = useGLTF("/suzanne.gltf");
-  return (
-    <group {...props} dispose={null}>
-      <mesh
-        castShadow
-        receiveShadow
-        geometry={nodes.Suzanne.geometry}
-        material={nodes.Suzanne.material}
-        position={[0, 0.189, -0.043]}
-      />
-    </group>
-  );
-}
-
-useGLTF.preload("/suzanne.gltf");
-
 export default Main;
 
