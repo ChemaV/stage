9e2817a8d7c9a05f636fe51fc39b1e087f42a2ee
ChemaV <quemavic@gmail.com>
2024-01-30 08:43:05 +0100

    daily push

--- src/app/components/Nav.jsx
+++ src/app/components/Nav.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React from 'react';
+import Link from 'next/link';
 
-export default function Nav() {
+const Nav = () => {
   return (
     <div>
       <h1>Post Organic Club</h1>
+      <nav>
+        <ul className="flex">
+          <li className="flex-1 mr-2">
+            <Link href="/tonight">
+              <a className="text-center block border border-white rounded py-2 px-4 bg-transparent hover:bg-white hover:text-blue-500">
+                Tonight Pava
+              </a>
+            </Link>
+          </li>
+          <li className="flex-1 mr-2">
+            <Link href="/post-organic-club">
+              <a className="text-center block border border-white rounded py-2 px-4 bg-transparent hover:bg-white hover:text-blue-500">
+                Post Organic Club
+              </a>
+            </Link>
+          </li>
+          <li className="flex-1 mr-2">
+            <Link href="/next-week">
+              <a className="text-center block border border-white rounded py-2 px-4 bg-transparent hover:bg-white hover:text-blue-500">
+                Next week
+              </a>
+            </Link>
+          </li>
+        </ul>
+      </nav>
     </div>
-  )
-}
+  );
+};
+
+export default Nav;
+
--- src/app/page.js
+++ src/app/page.js
@@ -1,17 +1,29 @@
+// Importa las dependencias necesarias
+import React from 'react';
 import Alert from 'react-bootstrap/Alert';
+import Nav from './components/Nav';
 
-function BasicExample() {
+
+const MyPage = () => {
   return (
     <>
-      {[
-        'primary',
-      ].map((variant) => (
-        <Alert key={variant} variant={variant}>
-          Tonight Diva Plavalaguna
-        </Alert>
-      ))}
+      {/* Contenido principal de la página */}
+      <section>
+        {/* Componente de alerta */}
+        {['primary'].map((variant) => (
+          <Alert key={variant} variant={variant}>
+            Tonight Diva Plavalaguna
+          </Alert>
+        ))}
+
+        {/* Componente de Navbar */}
+        <Nav />
+      </section>
     </>
   );
-}
+};
 
-export default BasicExample;
+export default MyPage;
+
+
+
